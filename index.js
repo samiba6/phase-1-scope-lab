57b4bab8407865c4dac30dfdb125d32404c2f344
Sami <s.babouche@esi-sba.dz>
2023-09-11 17:32:13 +0100

    Completed Assingment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+// Function to return the customerName.
+
+function upperCaseCustomerName() {
+    customerName=customerName.toUpperCase();
+  return customerName
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Output: 'not bob'
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+
+
+ // Output: 'maybe bob'
+
+const leastFavoriteCustomer = 'Alice';
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Bob'; // This will result in an error.
+}
+
+// Call the function (will throw a TypeError).
